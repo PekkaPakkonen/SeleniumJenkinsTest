a77029e1f97020c4c0100e2c7f78550521c8fcd8
PekkaPakkonen <redxray2010@gmail.com>
2022-06-09 14:09:05 +0300

    first upload

---
+++ src/test/java/org/ekfgroup/automation/SeleniumTest.java
@@ -0,0 +1,26 @@
+package org.ekfgroup.automation;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @BeforeTest
+    public void before() {
+
+    }
+
+    @Test
+    public void browse() {
+
+    }
+
+    @AfterTest
+    public void finish() {
+        
+    }
+
+}
+
+
