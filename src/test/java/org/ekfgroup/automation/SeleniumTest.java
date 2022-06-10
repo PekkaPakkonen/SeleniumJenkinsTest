8718e5175d194611a08077366886f21a74dfec03
PekkaPakkonen <redxray2010@gmail.com>
2022-06-10 14:09:56 +0300

    upd selenium

--- src/test/java/org/ekfgroup/automation/SeleniumTest.java
+++ src/test/java/org/ekfgroup/automation/SeleniumTest.java
@@ -1,18 +1,24 @@
 package org.ekfgroup.automation;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.net.URL;
 
 public class SeleniumTest {
 
     WebDriver driver;
 
     @BeforeTest
-    public void before() {
-        driver = new FirefoxDriver();
+    public void before() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("firefox");
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps);
     }
 
     @Test
