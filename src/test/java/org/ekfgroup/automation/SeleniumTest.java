7d2a41e66b455b5ff62b63cd0af17c26f6c9b424
PekkaPakkonen <redxray2010@gmail.com>
2022-06-10 10:24:55 +0300

    updated pom

--- src/test/java/org/ekfgroup/automation/SeleniumTest.java
+++ src/test/java/org/ekfgroup/automation/SeleniumTest.java
@@ -1,19 +1,23 @@
 package org.ekfgroup.automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
+    WebDriver driver;
+
     @BeforeTest
     public void before() {
-
+        driver = new FirefoxDriver();
     }
 
     @Test
     public void browse() {
-
+             driver.get("https://ekfgroup.com/");
     }
 
     @AfterTest
